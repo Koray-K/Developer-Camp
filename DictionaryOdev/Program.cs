5178c8406816856c99cb7f227a68fa1238bd5340
Koray-K <69123564+Koray-K@users.noreply.github.com>
2021-02-03 18:45:14 +0300

    Odev eklendi

---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sozluk = new MyDictionary<int, string>();
+
+            sozluk.Add(1, "KORAY");
+            sozluk.Add(2, "Aybar");
+
+            for (int i = 0; i < sozluk.Keys.Length; i++)
+            {
+                Console.WriteLine(sozluk.Keys[i] + " " + sozluk.Values[i]);
+
+            }
+        }
+    }
+}
